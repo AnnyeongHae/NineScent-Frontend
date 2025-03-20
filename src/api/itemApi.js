429a5ef13c485c9722fb0f00d0c47fe99c84bf8f
CoderAnnyeong <andrew4may@gmail.com>
2025-03-20 22:34:38 +0900

    Update itemApi.js

--- src/api/itemApi.js
+++ src/api/itemApi.js
@@ -1,4 +1,13 @@
-import api from '@/api';
+import axios from 'axios';
+
+Axios 인스턴스 생성
+const api = axios.create({
+    baseURL: '/api', // 프록시 경로와 일치
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
 
 export default {
   //    모든 상품 조회
